a2df61dd7b0546e7a5de26eacf05c66431bae986
supersonic <guptasanchit16@gmail.com>
2020-04-26 18:25:01 +0530

    Update settings.py

--- dapjakaws/settings.py
+++ dapjakaws/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'new',
         'USER': 'my',
-        'PASSWORD': 'malecode',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
