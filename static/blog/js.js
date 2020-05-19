dca971f7d380bb9032fd029a1c945037072a4383
supersonic16 <guptasanchit16@gmail.com>
2020-05-19 18:33:11 +0530

    added linebreaks

--- static/blog/js.js
+++ static/blog/js.js
@@ -1,4 +1,47 @@
-function showSearch1(){
-  console.log("hey");
+/* jquery code */
+$(document).ready(function(){
 
-}
+  if ($(window).width() < 992){
+    $("#navbrand").removeClass("pl-2");
+    $("#myForm").addClass("w-100");
+
+  }
+  else if (searchText.value == false){
+    $("#searchText").hide();
+  }
+  else{
+      $("#searchText").show();
+  }
+  $("#searchButton").click(function(){
+    var searchText= document.getElementById("searchText");
+    if (searchText.value == false){
+      $("#searchText").toggle();
+      $("#myForm").submit(function(event){
+        event.preventDefault();
+      });
+    }
+    else {
+      $("#myForm").unbind('submit');
+      $("#searchText").show();
+    }
+  });
+  $(window).resize(function(){
+      if ($(window).width() < 992){
+        $("#mydiv").removeClass("input-group flex-box");
+        $("#navbrand").removeClass("pl-2");
+        $("#myForm").addClass("w-100");
+      }
+      else{
+        $("#mydiv").addClass("input-group flex-box");
+        $("#navbrand").addClass("pl-2");
+        $("#myForm").removeClass("w-100");
+      }
+  });
+
+  $("#collapseButton").click(function(){
+      $("#searchText").show();
+      $("nav").removeClass("fixed-top");
+      $("#topNav").addClass("scrollTop");
+  });
+
+});
