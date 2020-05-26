3da800c6cad92bdbe4c5ee1aba9d5b18985e0014
supersonic16 <guptasanchit16@gmail.com>
2020-05-26 12:49:29 +0530

    Bytes IO update

--- users/models.py
+++ users/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image=models.ImageField(default='default.png', upload_to='profile_pics')
