503d6cecd7ada09f9c40c18e13260e3d8acb7fc6
supersonic16 <guptasanchit16@gmail.com>
2020-06-14 16:47:58 +0530

    Changed contact model

--- blog/forms.py
+++ blog/forms.py
@@ -23,35 +23,3 @@ class NameForm(forms.ModelForm):
     class  Meta:
         model=NameModel
         fields=('name','email')
-
-class ContactForm(forms.ModelForm):
-    name=forms.CharField(max_length=100, widget=forms.TextInput(
-        attrs={
-            'class': 'form-control mb-4',
-            'id': 'defaultContactFormName',
-            'placeholder': "Name",
-            'required': 'required'
-        }
-    ))
-
-    email=forms.CharField(max_length=100, widget=forms.EmailInput(
-        attrs={
-            'class': 'form-control mb-4',
-            'id': 'defaultContactFormEmail',
-            'placeholder': "Email",
-            'required': 'required'
-        }
-    ))
-
-    message=forms.CharField(max_length=500, widget=forms.Textarea(
-        attrs={
-            'class': 'form-control mb-4',
-            'id': 'defaultContactFormMessage',
-            'placeholder': "Message",
-            'row': '5'
-        }
-    ))
-
-    class  Meta:
-        model=ContactModel
-        fields=('name','email','message')
